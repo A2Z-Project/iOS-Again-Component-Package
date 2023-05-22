6d10c7feb75f1106052993349a32063fd2633977
DDang <k3h1i013@gmail.com>
2023-05-23 08:57:50 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+> Again(iOS 플랫폼)에 포함되는 디자인 컴포넌트를 모듈화하여 Package 형태로 분리하였습니다.
+
+# Package Info.
+- 지원 플랫폼(버전): iOS 15.0 및 상위 버전
+
+# Package Rules.
+- 각 View Class의 접두사에는 프로젝트 고유 접두사로서 `A2Z-Project`의 약어로 `AZ`를 사용한다.
+Ex) `AZButton`, `AZTextField`, `AZText`
---
+++ Tests/iOS-Again-Component-PackageTests/iOS_Again_Component_PackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iOS_Again_Component_Package
+
+final class iOS_Again_Component_PackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iOS_Again_Component_Package().text, "Hello, World!")
+    }
+}
