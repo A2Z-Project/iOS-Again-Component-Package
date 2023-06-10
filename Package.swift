d07d154dbb409cab869877a2304378b9f4e2bf6e
DDang <k3h1i013@gmail.com>
2023-06-11 01:38:04 +0900

    Updated Package Library Name (#5)

--- Package.swift
+++ Package.swift
@@ -12,8 +12,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "iOS-Again-Component-Package",
-            targets: ["iOS-Again-Component-Package"]),
+            name: "APUtil",
+            targets: ["Util"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
